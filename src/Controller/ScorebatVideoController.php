dc5c665aec937fc75827357cee553b4dd5e1f659
Jose Manuel <josemanuel.montero@agiliacenter.com>
2024-04-04 14:41:57 +0200

    Feat: Add ScorebatVideoController

---
+++ src/Controller/ScorebatVideoController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Services\ScorebatVideoService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ScorebatVideoController extends AbstractController
+{
+    public function __construct(private ScorebatVideoService $scorebatVideoService)
+    {
+        $this->scorebatVideoService = $scorebatVideoService;
+    }
+
+    #[Route('/', name: 'app_scorebat_video')]
+    public function index(): JsonResponse
+    {
+        return $this->json($this->scorebatVideoService->getRecentFeed(), Response::HTTP_OK);
+    }
+}
