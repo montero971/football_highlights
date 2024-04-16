d67229b3d30ba4263a66e81d29cd015ba5e560a8
Jose Manuel <josemanuel.montero@agiliacenter.com>
2024-04-16 13:00:55 +0200

    Feat: Main view

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Feed from "./components/Feed/Feed.jsx";
+import Logo from "./components/Logo/Logo.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Logo />
+    <Feed />
+  </React.StrictMode>
+);
